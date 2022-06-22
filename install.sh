b6996922fe05d09bcce29b4abf5991f78b939168
Carter Davis <carterdavis1928@gmail.com>
2022-06-22 15:37:04 +0100

    home dir dot files installer

--- install.sh
+++ install.sh
@@ -4,9 +4,18 @@ echo "Please run this script from within the configs directory. Press enter to b
 read
 
 CONFIG=".config/*"
+HOMEDFILES="./home/.[!.]*"
 
 for i in $CONFIG
 do
 	echo "Adding $i link.."
 	ln -s $(pwd)/$i $HOME/.config/
+done
+
+
+# Link dotfiles in home dir
+for i in $HOMEDFILES
+do
+	echo "Adding $i link.."
+	ln -s $(pwd)/$i $HOME/
 done
